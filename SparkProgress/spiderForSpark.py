471884a0d66cd367229d667d966f0c5481ca5da7
Cuiys <1209322594@qq.com>
2020-09-06 20:25:11 +0800

    已实现功能： 1. 每两秒刻画运行时的spark任务进度，非触发式的 2. 采集正在运行时的task启动时间,可用来计算kill操作的是loss 3. 修改为web触发式函数 还需实现： 1. 控制操作为controlSpark.py 但未整合 2. 没有获取读取检查点的开销 代码缺陷： 1. spark的活跃任务实际上不需要事实监控：     在 def getAppID_Port()函数中我们通过yarn application获取运行中的spark任务     因此不需要在设置线程去实时监控更新活跃的spark任务

--- SparkProgress/spiderForSpark.py
+++ SparkProgress/spiderForSpark.py
@@ -74,6 +74,99 @@ def getProgress(res):
     finally:
         return progress
 
+def getStageID(ip, app, port=18080):
+    '''
+    :param ip:
+    :param app:spark application
+    :param port:
+    :return:此时正在运行的stageID
+    '''
+    url = "http://{0}:{1}/api/v1/applications/{2}/stages".format(ip, port, app)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        response.encoding = response.apparent_encoding
+    except requests.exceptions.HTTPError as err:
+        print("访问失败{}".format(err))
+        return None
+    activeID = []
+    stageinfo = response.json()
+    for stage in stageinfo:
+        if stage["status"] == "PENDING":
+            activeID.append(stage["stageId"])
+    if len(activeID) == 0:
+        print( "spark任务{}运行结束".format(app))
+        return None
+    activeID.sort()
+    return activeID[0]
+
+def getRunningTask(app, stageID):
+    '''
+    :param app:
+    :param stageid:
+    :return: 正在运行的task的启动时间
+    '''
+    # "http://master:8088/proxy/application_1599144170737_0013/stages/stage/?id=0&attempt=0"
+    url = "http://master:8088/proxy/{0}/stages/stage/?id={1}&attempt=0".format(app, stageID)
+    runningtaskdata = []
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        response.encoding = response.apparent_encoding
+    except requests.exceptions.HTTPError as err:
+        print("无法获取任务{}的stage信息:{}".format(app, err))
+        return None
+    soup = BeautifulSoup(response.text.encode("utf-8"), 'lxml')
+    table = soup.find_all("table")
+    tasktable = table[2]
+    taskinfo = tasktable.find_all('tr')
+    for task in taskinfo:
+        l = task.find_all('td')
+        taskStatus = l[3].string
+        if taskStatus == "RUNNING":
+            taskid = l[1].string
+            address = l[6].div.string
+            startime = l[7].string
+            startime = int(time.mktime(time.strptime(startime, '%Y/%m/%d %H:%M:%S'))) * 1000
+            info = [taskid, taskStatus, address, startime]
+            runningtaskdata.append(info)
+
+    return runningtaskdata
+
+def Priority():
+    priority = []
+    for app in spark_appDict:
+        res = getResponse(ip, spark_appDict[app])
+        if res is None: continue
+        p = getProgress(res)
+        if "progress" not in p.keys(): continue
+        # print("Spark任务 {0} 的工作进度为：---{1:.2f}%---".format(app, p["progress"] * 100))
+        priority.append([app, p["progress"] * 100])
+        priority.sort(key=lambda x: x[1], reverse=False)
+        job_order = [x[0] for x in priority]
+    return priority
+
+
+
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+@app.route("/getSpark", methods=['GET'])
+def getSpark():
+    return jsonify(spark_appDict)
+
+@app.route('/getPriority', methods=["GET"])
+def getPriority():
+    '''
+    如果要设置多个K级队列，则将job_order进行划分
+    :return:
+    '''
+    priority = Priority()
+    job_order = {}
+    for i, app in enumerate(priority):
+        job_order[i] = app
+    return jsonify(job_order)
+
 
 
 def run(ip):
