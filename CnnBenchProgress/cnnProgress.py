934d0acc50898715f9dae8163fb9fd4b16d623d5
Cuiys <1209322594@qq.com>
2020-09-06 20:21:51 +0800

    web触发式获取Ai同步任务progress队列

--- CnnBenchProgress/cnnProgress.py
+++ CnnBenchProgress/cnnProgress.py
@@ -3,6 +3,14 @@ from scipy.optimize import curve_fit
 import os, re, time
 import subprocess
 
+'''
+功能；
+1. 对分布式同步训练任务的监控及进度刻画以及实现
+2. web触发式查询
+未完成：
+1. 缺乏控制程序
+2. 目前只能获取同步任务，缺乏对异步任务，非分布式任务的监控
+'''
 def getIns():
     cmd = ["docker", "exec", "-i", "Tensor-Worker-1", "bash", "/root/outins.sh"]
     info = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -48,34 +56,59 @@ def stepPredict(func, data_path, loss):
     progress = x[-1] / totalStep
     return totalStep, x[-1], progress
 
-def getPriority():
+def Priority():
     priority = []
-    while True:
-        insDict = getIns()
-        if insDict:
-            print("有任务在运行")
-            jobId = insDict.keys()
-            break
-        else:
-            print("没有任务在运行")
-            time.sleep(2)
-            continue
-    for i in jobId:
-        path = "/home/tank/cys/rhythm/BE/cnn-bench/CnnBenchProgress/cnn_appinfo/{}.txt".format(i)
-        if stepPredict(optimusFunc, path, 0.22) is None: 
-            print("训练任务{}已提交,但未开始训练".format(i))
-        totalStep, endstep, progress = stepPredict(optimusFunc, path, 0.22)
-        priority.append([i, progress])
+    # while True:
+    #     insDict = getIns()
+    #     if insDict:
+    #         print("有任务在运行")
+    #         jobId = insDict.keys()
+    #         break
+    #     else:
+    #         print("没有任务在运行")
+    #         time.sleep(2)
+    #         continue
+    insDict = getIns()
+    if insDict:
+        print("有任务在运行")
+        jobId = insDict.keys()
+        for i in jobId:
+            path = "/home/tank/cys/rhythm/BE/cnn-bench/CnnBenchProgress/cnn_appinfo/{}.txt".format(i)
+            if stepPredict(optimusFunc, path, 0.22) is None:
+                print("训练任务{}已提交,但未开始训练".format(i))
+            else:
+                totalStep, endstep, progress = stepPredict(optimusFunc, path, 0.22)
+                priority.append([i, progress])
 
-    priority.sort(key=lambda x: x[1], reverse=False)
-    return priority
+        priority.sort(key=lambda x: x[1], reverse=False)
+        return priority
+    else:
+        print("没有任务在运行")
+        return None
 
 
 
-if __name__ == '__main__':
-    while True:
-        priority = getPriority()
-        job_order = [x[0] for x in priority]
-        for x in priority: x[1] = "{:.2f}%".format(float(x[1]) * 100)
-        print("Progress 动态队列:", priority)
-        time.sleep(2)
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route("/getPriority", methods=["GET"])
+def getPriority():
+    priority = Priority()
+    job_order = {}
+    if priority:
+        for i, app in enumerate(priority):
+            job_order[i] = app
+    return jsonify(job_order)
+@app.route("/index", methods=["GET"])
+def index():
+    return "启动成功"
+app.run(host="0.0.0.0",port=10088)
+
+
+# if __name__ == '__main__':
+#     while True:
+#         priority = Priority()
+#         if priority:
+#             job_order = [x[0] for x in priority]
+#             for x in priority: x[1] = "{:.2f}%".format(float(x[1]) * 100)
+#             print("Progress 动态队列:", priority)
+#             time.sleep(2)
