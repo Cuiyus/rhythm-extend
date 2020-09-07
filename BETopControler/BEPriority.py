1c5b8ec8e1111b2a44c52b43df5960efef5ee509
Cuiys <1209322594@qq.com>
2020-09-07 14:58:06 +0800

    总控制器 -- Sci

--- BETopControler/BEPriority.py
+++ BETopControler/BEPriority.py
@@ -11,3 +11,34 @@ from scipy.optimize import curve_fit
 不可预测：SCIMARK
 '''
 # SCIMARK
+from HpccProgress.SerTime import scimarkProgress, scimarkHandler, Observer
+from HpccProgress.SerTime import resource
+sci = scimarkProgress()
+sci.event_handler = scimarkHandler(sci.appDict)
+sci.observer = Observer()
+sci.run()
+
+# Spark
+from SparkProgress.spiderForSpark import sparkProgress
+saprk = sparkProgress("192.168.1.106")
+
+# AI
+from CnnBenchProgress.cnnProgress import cnnProgress
+cnn = cnnProgress()
+
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/getPriority', methods=["GET"])
+def getPriority():
+    # sci
+    unpredict = {}
+    for pid in sci.appDict:
+        localtime = int(time.time()*1000) # 毫秒
+        cpunum = resource("Scimark")
+        sertime = (localtime - int(sci.appDict[pid][0])) / 1000
+        sci.appDict[pid].append([sertime, cpunum])
+        unpredict[pid] = sertime * cpunum
+    return unpredict
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=10089)
