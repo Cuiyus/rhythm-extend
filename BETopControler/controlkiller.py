a6347956baea466e4594a37de64aab45c035235e
Cuiys <1209322594@qq.com>
2020-09-20 22:29:12 +0800

    测试能否正常返回spark任务的executor信息

--- BETopControler/controlkiller.py
+++ BETopControler/controlkiller.py
@@ -43,7 +43,7 @@ class SparkKiller(object):
             cmd = "docker exec -i {} lsof -i:{}".format(self.worker, nodeinfo[1])
             print(cmd)
             pidfinfo = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE)
-            info = pidfinfo.stdout.decode('utf-8').split('\n')
+            info = pidfinfo.stdout.decode('utf-8').split('\n')[-2]
             print("---------info", info)
             executorPidPat = re.compile(r'java (\d{3,5}) root')
             executorPid = executorPidPat.findall(info)[0]
