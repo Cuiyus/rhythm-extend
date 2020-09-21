462fb75f237f79ab8f2a1d433195bc32445caf26
Cuiys <1209322594@qq.com>
2020-09-21 10:36:11 +0800

    解决kill某个job后无法获取progress问题； 问题原因：当killjob时，sparkui的progress进度栏中的running被替换为failed导致正则表达式无法匹配到。

--- SparkProgress/spiderForSpark.py
+++ SparkProgress/spiderForSpark.py
@@ -103,35 +103,24 @@ class sparkProgress(object):
         except requests.exceptions.ConnectionError:
             print("任务：{} --- 连接错误".format(app[0]))
             return None
-
-        soup = BeautifulSoup(res.text.encode("utf-8"), 'lxml')
-        res1 = soup.select('span[style="text-align:center; position:absolute; width:100%; left:0;"]')
         progress = {}
-        compilejob_info = {}
-        running_job, compile_job = 0, 0
+        running_job, compile_job, total_job = 0, 0, 0
+        soup = BeautifulSoup(res.text.encode("utf-8"), 'lxml')
+        activateJobInfo = soup.find("table", attrs={'id': "activeJob-table"}). \
+            select('span[style="text-align:center; position:absolute; width:100%; left:0;"]')
+        completedJobInfo = soup.find("table", attrs={'id': "completedJob-table"}). \
+            select('span[style="text-align:center; position:absolute; width:100%; left:0;"]')
+
         task_pat = re.compile(r'(\d{1,3})/(\d{1,3})')
-        for _ in res1:
-            if "running" in str(_):
-                running_job += 1
-                p = task_pat.search(str(_))
-                progress[running_job] = {}
-                progress[running_job]["running_task"] = int(p.group(1))
-                progress[running_job]["total_task"] = int(p.group(2))
-            else:
-                compile_job += 0
-                p = task_pat.search(str(_))
-                compilejob_info[compile_job] = {}
-                compilejob_info[compile_job]["total_task"] = int(p.group(2))
-                compilejob_info[compile_job]["progress"] = "100%"
-
-        running_task = 0
-        total_task = 0
-
-        for task in progress.values():
-            running_task += task["running_task"]
-            total_task += task["total_task"]
+        runningJobTaskInfo = task_pat.findall(activateJobInfo[0].string)[0]
+        completedJobTaskInfo = task_pat.findall(completedJobInfo[0].string)
+        for _ in completedJobTaskInfo:
+            total_job += int(_[0])
+        # 已运行的Job所有Task之和
+        total_job += int(runningJobTaskInfo[1])
+        running_job = int(runningJobTaskInfo[0])
         try:
-            progress["progress"] = running_task / total_task
+            progress["progress"] = running_job / int(runningJobTaskInfo[1])
         except ZeroDivisionError:
             print("The spark Job提交后未完成任务初始化，Total Task= 0")
             return None
