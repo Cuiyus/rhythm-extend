3a92d1e188924551166d36c227c7ac7a73d2b3de
Cuiys <1209322594@qq.com>
2020-10-05 22:18:51 +0800

    sci控制脚本更新

---
+++ control-scripts/addBeCopy_sci.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+# 运行scimark
+# 放置于容器中
+java -jar /root/UpdateScimark2.0.jar 2 >> /dev/null &
+starttime=$(date +%s%3N)
+echo  "$! $starttime" >> /share/sci.log
