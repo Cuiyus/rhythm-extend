1923d2ad304d6a1f9306c0be76c82375a25b1a76
Cuiys <1209322594@qq.com>
2020-10-05 20:59:04 +0800

    控制脚本更新

--- control-scripts/addBeCopy_spark.sh
+++ control-scripts/addBeCopy_spark.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-n=$RANDOM
+sparkApp=(KMenas LogisticRegression)
+function parity() {
+    local random=$1
+    if [ $[$random%2] -eq 0 ]
+    then
+      return 0;
+    else
+      return 1;
+    fi
 
-
-bash /root/spark-bench-legacy/bin/submit_job.sh KMeans LogisticRegression
+}
+function test() {
+    echo $1
+}
+parity $RANDOM
+bash /root/spark-bench-legacy/bin/submit_job.sh ${sparkApp[$?]}
