e6b26c7b8f57dc9f0233f83b61eccb2a8597fb57
Cuiys <1209322594@qq.com>
2020-10-06 15:43:19 +0800

    控制脚本更新

--- control-scripts/killAll.sh
+++ control-scripts/killAll.sh
@@ -4,6 +4,7 @@ ps -ef | grep l3|cut -c 9-15|xargs kill -9
 ps -ef | grep cpu|cut -c 9-15|xargs kill -9
 ps -ef | grep python|cut -c 9-15|xargs kill -9
 ps -ef | grep single|cut -c 9-15|xargs kill -9
+ps -ef | grep java|cut -c 9-15|xargs kill -9
 applist=$(yarn application -list | grep http | grep application | awk '{print $1}' )
 for app in $applist; do
   echo "Delete Job $app"
